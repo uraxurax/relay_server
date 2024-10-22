46bcd1d35a26ccfb0da8dee9833973a509fe85a2
Hiromitsu Matsuura <uraxurax@gmail.com>
2024-10-23 07:23:42 +0900

    Update nanosec code

--- main.c
+++ main.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+#include <time.h>
+#include <stdint.h>
+#include <inttypes.h>
 
 
 int main()
 {
-    printf("Hello World\n");
+    uint64_t nanosec;
 
+    struct timespec real;
 
+    clock_gettime(CLOCK_REALTIME, &real);
+    nanosec = (uint64_t)real.tv_sec * 1000000000 + real.tv_nsec;
 
+    printf("%lu %lu\n", real.tv_sec, real.tv_nsec);
+    printf("0x%lx 0x%lx\n", real.tv_sec, real.tv_nsec);
 
+    // printf("%lu\n", nanosec);
+    printf("PRIu64 = %" PRIu64 "\n", nanosec);
 
     return 0;
 }
+
+
+
